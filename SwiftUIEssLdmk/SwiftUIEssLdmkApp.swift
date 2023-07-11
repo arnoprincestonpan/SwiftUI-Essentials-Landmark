730d7491449df8ebb57c5b5439a341b6e4d2ef6c
Arno Pan <apan11@my.bcit.ca>
2023-07-11 13:34:39 -0700

    Initial Commit

---
+++ SwiftUIEssLdmk/SwiftUIEssLdmkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssLdmkApp.swift
+//  SwiftUIEssLdmk
+//
+//  Created by Arno Pan on 2023-07-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssLdmkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
