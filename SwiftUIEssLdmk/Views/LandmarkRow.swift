7b4f59f6f89195052ddcc66626a8ea45b649a18a
Arno Pan <apan11@my.bcit.ca>
2023-07-11 17:01:41 -0700

    building lists and navigation

---
+++ SwiftUIEssLdmk/Views/LandmarkRow.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkRow.swift
+//  SwiftUIEssLdmk
+//
+//  Created by Arno Pan on 2023-07-11.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 48, height: 48)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 296, height: 68))
+    }
+}
