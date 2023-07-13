91d28166d1f32f5d2bddc32fda93b712275ee3d5
Arno Pan <apan11@my.bcit.ca>
2023-07-12 17:18:23 -0700

    rearranged app

--- SwiftUIEssLdmk/Views/LandmarkList.swift
+++ SwiftUIEssLdmk/Views/LandmarkList.swift
@@ -8,29 +8,43 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = false 
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label : {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label : {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
-            .listStyle(PlainListStyle())
             .navigationTitle("Landmarks")
+            .listStyle(PlainListStyle())
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (3rd generation)", "iPhone 14"], id: \.self) { deviceName in
-                 LandmarkList()
-                     .previewDevice(PreviewDevice(rawValue: deviceName))
-                     .previewDisplayName(deviceName)
-             }
+//        ForEach(["iPhone SE (3rd generation)", "iPhone 14"], id: \.self) { deviceName in
+//            LandmarkList()
+//                .environmentObject(ModelData())
+//             }
+        LandmarkList()
+            .environmentObject(ModelData())
     }
 }
